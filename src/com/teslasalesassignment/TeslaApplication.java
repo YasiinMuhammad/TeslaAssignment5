e758f99091acaa0c184f887e4fa1ddccfabb34d5
Yasiin <yasiinmuhammad@gmail.com>
2020-08-29 18:47:42 -0700

    Update

--- src/com/teslasalesassignment/TeslaApplication.java
+++ src/com/teslasalesassignment/TeslaApplication.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional; 
+import java.util.Optional;
 import java.util.stream.Collectors;
-
 
 public class TeslaApplication {
 
@@ -29,30 +28,28 @@ public class TeslaApplication {
 				return o1.getSales().compareTo(o2.getSales());
 			}
 		};
+
 		Optional<SalesData> minNumber = teslaCarSales.stream().min(minComparator);
 		System.out.println("The worst month for " + modelType + " was: " + minNumber.get());
-		
+
 		Comparator<SalesData> maxComparator = new Comparator<SalesData>() {
 			@Override
 			public int compare(SalesData o1, SalesData o2) {
 				return o1.getSales().compareTo(o2.getSales());
 			}
 		};
-		Optional<SalesData> maxNumber = teslaCarSales.stream().max(maxComparator);
 
+		Optional<SalesData> maxNumber = teslaCarSales.stream().max(maxComparator);
 		System.out.println("The best month for " + modelType + " was: " + maxNumber.get());
-		 Map<Object, List<SalesData>> grouped = teslaCarSales.stream()
-			        .map(x -> x)
-			        .collect(Collectors.groupingBy(x->x.getDate().getYear()));
-			    grouped
-			        .entrySet()
-			        .stream();
-			    
-			    String totalTeslaSales =grouped.entrySet()
-			    	.stream().map(x ->x.getKey() + " ->" + x.getValue().stream()
-			    	.collect(Collectors.summingInt(SalesData::getSales)))
-			    	.collect(Collectors.joining("\n"));
-			    		System.out.println(totalTeslaSales);
+
+		Map<Object, List<SalesData>> grouped = teslaCarSales.stream().map(x -> x)
+				.collect(Collectors.groupingBy(x -> x.getDate().getYear()));
+		grouped.entrySet().stream();
+
+		String totalTeslaSales = grouped.entrySet().stream().map(
+				x -> x.getKey() + " ->" + x.getValue().stream().collect(Collectors.summingInt(SalesData::getSales)))
+				.collect(Collectors.joining("\n"));
+		System.out.println(totalTeslaSales);
 	}
 
 }
